d205e2876ecf9567c16cc2aac50ff24e32087c9a
vzirshehryar <vzirshehryar@gmail.com>
2024-05-10 15:58:53 +0500

    fixed some build issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-import Explore from "./components/Explore";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import OxAuth from "./components/OxAuth";
+import Explore from "../components/Explore";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
